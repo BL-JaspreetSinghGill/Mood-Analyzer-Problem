fb40392f055f3a6da11c47f40be3e23460dfef74
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-02-05 10:39:33 +0530

    REFACTORED: the code to take mood message in constructor

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,27 +1,21 @@
 package com.bridgelabz.moodanalyser;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
-    private MoodAnalyser moodAnalyser;
-
-    @Before
-    public void initializer() {
-        moodAnalyser = new MoodAnalyser();
-    }
-
     @Test
     public void givenMessage_WhenContainsSad_ShouldReturnSad() {
-        String mood = moodAnalyser.analyseMood("I am in Sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
     public void givenMessage_WhenContainsAnyMood_ShouldReturnHappy() {
-        String mood = moodAnalyser.analyseMood("I am in any mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
 }
